516ba4d5e069dd83db4a6b83f6f49657b50a337b
Shivam Kulkarni <shivam.suyash@gmail.com>
2026-01-31 17:33:51 +0530

    chore: products page

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 import { FilterSidebar } from '../../components/filter-sidebar/filter-sidebar.component';
+import { ProductCard } from '../../components/product-card/product-card.component';
+import { ProductService } from '../../core/services/product.service';
 
 @Component({
-  selector: 'component-products',
-  imports: [FilterSidebar],
+  selector: 'app-products',
+  imports: [CommonModule, FilterSidebar, ProductCard, MatIconModule],
   templateUrl: './products.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Products {}
+export class Products {
+  protected productService = inject(ProductService);
+}
